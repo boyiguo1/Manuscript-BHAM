8f6fd3600b7aec4a354cc966a9bfe349c887f82a
Boyi Guo <boyiguo1@uab.edu>
2021-06-02 11:31:52 -0500

    * Update manuscript writing * Set up targets for sub files

--- _targets.R
+++ _targets.R
@@ -25,6 +25,7 @@ list(
   
   #  Real Data Analysis -----------------------------------------------------
   #* Emory Card Biobank ####
+  #** Load Data ####
   tar_target(realdata_ECB_train_path,
              "Real_Data/Emory_Card_Biobank/Data/Analysis_data_first_cohort.csv",
              format = "file"),
@@ -35,14 +36,47 @@ list(
              readr::read_csv(realdata_ECB_train_path)),
   tar_target(RD_ECB_valid_dat,
              readr::read_csv(realdata_ECB_valid_path)), 
-  tar_render(real_data_report_ECB,
-             "Real_Data/Emory_Card_Biobank/Report.Rmd",
-             output_dir = "Real_Data/Emory_Card_Biobank/",
-             output_file = "ECB_report.html"),
-  
+  #** Report ####
+  # tar_render(real_data_report_ECB,
+  #            "Real_Data/Emory_Card_Biobank/Report.Rmd",
+  #            output_dir = "Real_Data/Emory_Card_Biobank/",
+  #            output_file = "ECB_report.html"),
+  # 
   
   # Manuscript --------------------------------------------------------------
+
+  #* Section Paths ####
+  tar_target(intro_path,
+             "Manuscript/01-Intro.Rmd",
+             format = "file"),
+  
+  tar_target(method_path,
+             "Manuscript/02-Method.Rmd",
+             format = "file"),
+  
+  tar_target(sim_path,
+             "Manuscript/03-Simulation.Rmd",
+             format = "file"),
+  
+  tar_target(real_data_path,
+             "Manuscript/04-Real_Data.Rmd",
+             format = "file"),
+  
+  tar_target(disc_path,
+             "Manuscript/05-Discussion.Rmd",
+             format = "file"),
+  tar_target(fig_path,
+             "Manuscript/Fig/",
+             format = "file"),
+  
   #* Assemble Manuscript ####
-  tar_render(manu, "Manuscript/main.Rmd", output_file = "SS_GAM.pdf")
+  tar_render(manu, "Manuscript/main.Rmd", 
+             # params = list(
+             #   intro = intro_path,
+             #   method = method_path,
+             #   sim = sim_path,
+             #   RD = real_data_path,
+             #   disc = disc_path),
+             output_file = "SS_GAM.pdf")
   
 )
