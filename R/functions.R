6bc7c27749f6c5c2226a6bf1903f0eb44f1535bf
Boyi Guo <boyiguo1@uab.edu>
2021-06-20 19:15:16 -0500

    Add more content

---
+++ R/functions.R
@@ -0,0 +1,169 @@
+#' Use GAM to screen the features
+#'
+#' @param dat the raw data
+#' @param nrow How many rows are selected
+#'
+#' @return
+#'
+gam_screen <- function(dat, nrow = 200){
+  dat %>% 
+    select(starts_with("mz")) %>% 
+    names() %>% 
+    map_dfr(#feature_name[1:2],
+      .f = function(name, .dat){
+        # name <- feature_name[1]
+        y <- .dat %>% pull(death3yr)
+        x <- .dat %>% pull({{name}})
+        mgcv::gam(y ~ s(x, bs = "cr", k = 10), family = binomial(),
+                  data = data.frame(x, y)) %>% 
+          tidy() %>% 
+          filter(term == "s(x)") %>% 
+          select(p.value) %>% 
+          mutate(var = name,
+                 p.value)
+        
+      },
+      .dat = dat) %>% data.frame(
+        .,
+        p.adj = p.adjust(.$p.value, "fdr")
+      ) %>% arrange(p.adj) %>% #filter(p.adj<0.2)
+    head(nrow)
+}
+
+
+create_success_rate_table <- function(path){
+  sims <- list.files(path,full.names =TRUE)
+  
+  success_rate <- map_dfr(sims, .f = function(sim){
+    sim.df <- unglue_data(sim, 
+                          "{}/bgam_{study}_-dis_{dist}-p_{p}") %>% 
+      mutate( p = as.numeric(p))
+    fls <- list.files(sim, full.names = TRUE) 
+    fls <- fls[grep(".rds", x=fls)]
+    n <- length(fls)
+    
+    data.frame(sim.df, n_success = n, path = sim)
+  }) %>% 
+    arrange(p) 
+
+  return(success_rate)  
+}
+
+
+make_sim_main_plots <- function(success_rate, dist, measures){
+  total_dat <- success_rate %>% 
+    # filter(dist==dist, n_success!=0) %>% 
+    filter(dist=={{dist}}, n_success!=0) %>%
+    pull(path) %>% 
+    map_dfr( .f = function(sim){
+      sim.df <- unglue_data(sim, 
+                            "{}/bgam_{study}_-dis_{dist}-p_{p}") %>% 
+        mutate( p = as.numeric(p))
+      fls <- list.files(sim, full.names = TRUE) 
+      fls <- fls[grep(".rds", x=fls)]
+      n <- length(fls)
+      
+      
+      if(n == 0)
+        return(data.frame(NULL))
+      
+      # .file <- fls[1]
+      
+      ret <- fls %>%
+        map_dfr(.f = function(.file){
+          it <- unglue_data(.file, "{whatever}/it_{it}.rds") %>% pull(it) %>% as.numeric
+          ret <- data.frame(
+            it = it,
+            read_rds(.file) %>% 
+              data.frame() %>% 
+              rownames_to_column("method")
+
+          )
+        }) %>%
+        arrange(it) %>% 
+        # arrange(mdl, s0) %>% 
+        mutate(#s0 = factor(s0),
+          p = sim.df$p)#  %>% 
+      
+    }) %>% 
+    mutate(method = factor(method, levels = c("bglm_t", "bglm_de", "blasso",
+                                              "bglm_spline_t", "bglm_spline_de", "blasso_spline",
+                                              "cosso", "acosso", "mgcv"
+    )))
+  
+  
+  measures %>%
+    map(.f = function(.name){
+      total_dat %>% 
+        select(p, method, starts_with("test.")) %>% #head
+        pivot_longer(cols = starts_with("test.")) %>% 
+        filter(name == {{.name}}) %>%
+        # filter(name == {{measures}}) %>%
+        ggplot(#data = binom_total_dat,
+          aes(x = method, y = value)) +
+        geom_jitter() +
+        geom_boxplot() +
+        theme(axis.text.x = element_text(angle = 45))+
+        # coord_flip() +
+        facet_grid(cols = vars(p),
+                   rows= vars(name)#,
+                   # scale = "free_y"
+                   )
+    }) %>%
+  ggarrange(plotlist = ., ncol = 1)
+}
+
+make_sim_table <- function(success_rate, dist#, measures
+                           ){
+  total_dat <- success_rate %>% 
+    # filter(dist==dist, n_success!=0) %>% 
+    filter(dist=={{dist}}, n_success!=0) %>%
+    pull(path) %>% 
+    map_dfr( .f = function(sim){
+      sim.df <- unglue_data(sim, 
+                            "{}/bgam_{study}_-dis_{dist}-p_{p}") %>% 
+        mutate( p = as.numeric(p))
+      fls <- list.files(sim, full.names = TRUE) 
+      fls <- fls[grep(".rds", x=fls)]
+      n <- length(fls)
+      
+      
+      if(n == 0)
+        return(data.frame(NULL))
+      
+      # .file <- fls[1]
+      
+      ret <- fls %>%
+        map_dfr(.f = function(.file){
+          it <- unglue_data(.file, "{whatever}/it_{it}.rds") %>% pull(it) %>% as.numeric
+          ret <- data.frame(
+            it = it,
+            read_rds(.file) %>% 
+              data.frame() %>% 
+              rownames_to_column("method")
+            
+          )
+        }) %>%
+        arrange(it) %>% 
+        # arrange(mdl, s0) %>% 
+        mutate(#s0 = factor(s0),
+          p = sim.df$p)#  %>% 
+      
+    }) %>% 
+    mutate(method = factor(method, levels = c("bglm_t", "bglm_de", "blasso",
+                                              "bglm_spline_t", "bglm_spline_de", "blasso_spline",
+                                              "cosso", "acosso", "mgcv"
+    )))
+  
+  
+      total_dat %>% 
+        select(p, method, starts_with("test.")) %>% #head
+        # pivot_longer(cols = starts_with("test.")) %>%
+        # mutate(name = str_remove(name, pattern = fixed("test."))) %>% 
+        rename_at(vars(dplyr::starts_with("test.")), ~str_remove(., pattern = fixed("test."))) %>%
+        # select(p,method, {{measures}}) %>% 
+        mutate(p = factor(p)) %>% 
+        group_by(p, method) %>% 
+        dplyr::summarise(across(everything(),list(mean = mean, sd = sd), na.rm = TRUE, .names = "{.col}.{.fn}")) %>% 
+        ungroup
+}
--- _targets.R
+++ _targets.R
@@ -1,6 +1,8 @@
 library(targets)
 library(tarchetypes)
 
+source("R/functions.R")
+
 # This is an example _targets.R file. Every
 # {targets} pipeline needs one.
 # Use tar_script() to create _targets.R and tar_edit()
@@ -10,18 +12,46 @@ library(tarchetypes)
 
 options(tidyverse.quiet = TRUE)
 # Set target-specific options such as packages.
-tar_option_set(packages = c("dplyr", "tidyverse", "rticles", "gtsummary", "here"))
+tar_option_set(packages = c("dplyr", "tidyverse", "rticles", "gtsummary", "here",
+                            "mgcv", "broom", "unglue", "knitr",
+                            "ggpubr"))
 
 # End this file with a list of target objects.
 list(
-
+  
   #  Simulations Studies -----------------------------------------------------
+  #* Main Study ####
+  #* 
+  #* 
+  tar_target(success_rate,
+             create_success_rate_table("Simulation/Main/Data/main_k=10/")),
+  
+  tar_target(binom_plot,
+             make_sim_main_plots(
+               success_rate,
+               "binomial",
+               list("test.deviance", "test.auc", "test.misclassification"))
+             ),
+
+  tar_target(gaussian_plot,
+             make_sim_main_plots(
+               success_rate,
+               "gaussian",
+               list("test.R2", "test.mse", "test.mae"))),
+
+  tar_target(poisson_plot,
+             make_sim_main_plots(
+               success_rate,
+               "poisson",
+               list("test.deviance", "test.mse", "test.mae"))
+             ),
+  
   #* Tuning Study ####
   tar_render(sim_tuning_report,
              "Simulation/tuning/tuning_study_report.Rmd",
              output_dir = "Simulation/tuning/",
              output_file = "sim_tuning_report.html"),
-    
+  
   
   #  Real Data Analysis -----------------------------------------------------
   #* Emory Card Biobank ####
@@ -36,15 +66,20 @@ list(
              readr::read_csv(realdata_ECB_train_path)),
   tar_target(RD_ECB_valid_dat,
              readr::read_csv(realdata_ECB_valid_path)), 
+  
+  #** GAM Screening ####
+  tar_target(train_gam_screen,
+             gam_screen(RD_ECB_train_dat)),
+  
   #** Report ####
   tar_render(real_data_report_ECB,
              "Real_Data/Emory_Card_Biobank/Report.Rmd",
              output_dir = "Real_Data/Emory_Card_Biobank/",
              output_file = "ECB_report.html"),
-
+  
   
   # Manuscript --------------------------------------------------------------
-
+  
   #* Section Paths ####
   tar_target(intro_path,
              "Manuscript/01-Intro.Rmd",
