738a7f2d41b53120a2002b69db7d7b5380dbab06
Boyi Guo <boyiguo1@uab.edu>
2021-07-27 16:59:55 -0500

    Setup targets use tflow

--- R/functions.R
+++ R/functions.R
@@ -31,22 +31,43 @@ gam_screen <- function(dat, nrow = 200){
 }
 
 
-create_success_rate_table <- function(path){
-  sims <- list.files(path,full.names =TRUE)
-  
-  success_rate <- map_dfr(sims, .f = function(sim){
-    sim.df <- unglue_data(sim, 
-                          "{}/bgam_{study}_-dis_{dist}-p_{p}") %>% 
-      mutate( p = as.numeric(p))
-    fls <- list.files(sim, full.names = TRUE) 
-    fls <- fls[grep(".rds", x=fls)]
-    n <- length(fls)
-    
-    data.frame(sim.df, n_success = n, path = sim)
-  }) %>% 
-    arrange(p) 
+create_success_rate_table <- function(path_list){
 
-  return(success_rate)  
+  # sims <- list.files(path,full.names =TRUE)
+  # browser()
+  unglue_data(path_list[str_ends(path_list, ".rds")],
+              "{path}/it_{}") %>% 
+    group_by(path) %>% 
+    summarize(n_success = n()) %>% 
+    unglue_unnest(
+      col = path,
+      patterns = "{}/bgam_{study}_-dis_{dist}-p_{p}",
+      remove = FALSE
+    ) %>% 
+    mutate( p = as.numeric(p)) %>% 
+    arrange(dist, p) %>% 
+    select(dist, p, n_success, path)
+  #   head()
+  # 
+  # 
+  # success_rate <- map_dfr(path_list, 
+  #                         .f = function(sim_file){
+  #                           browser()
+  #   sim.df <- unglue_data(sim_file, 
+  #                         "{}/bgam_{study}_-dis_{dist}-p_{p}/{}", 
+  #                         kee) %>% 
+  #     mutate( p = as.numeric(p)) %>% 
+  #     group_by(dist)
+  #     summarize()
+  #   fls <- list.files(sim_file, full.names = TRUE) 
+  #   fls <- fls[grep(".rds", x=fls)]
+  #   n <- length(fls)
+  #   
+  #   data.frame(sim.df, n_success = n, path = sim)
+  # }) %>% 
+  #   arrange(p) 
+
+  # return(success_rate)  
 }
 
 
@@ -113,57 +134,3 @@ make_sim_main_plots <- function(success_rate, dist, measures){
   ggarrange(plotlist = ., ncol = 1)
 }
 
-make_sim_table <- function(success_rate, dist#, measures
-                           ){
-  total_dat <- success_rate %>% 
-    # filter(dist==dist, n_success!=0) %>% 
-    filter(dist=={{dist}}, n_success!=0) %>%
-    pull(path) %>% 
-    map_dfr( .f = function(sim){
-      sim.df <- unglue_data(sim, 
-                            "{}/bgam_{study}_-dis_{dist}-p_{p}") %>% 
-        mutate( p = as.numeric(p))
-      fls <- list.files(sim, full.names = TRUE) 
-      fls <- fls[grep(".rds", x=fls)]
-      n <- length(fls)
-      
-      
-      if(n == 0)
-        return(data.frame(NULL))
-      
-      # .file <- fls[1]
-      
-      ret <- fls %>%
-        map_dfr(.f = function(.file){
-          it <- unglue_data(.file, "{whatever}/it_{it}.rds") %>% pull(it) %>% as.numeric
-          ret <- data.frame(
-            it = it,
-            read_rds(.file) %>% 
-              data.frame() %>% 
-              rownames_to_column("method")
-            
-          )
-        }) %>%
-        arrange(it) %>% 
-        # arrange(mdl, s0) %>% 
-        mutate(#s0 = factor(s0),
-          p = sim.df$p)#  %>% 
-      
-    }) %>% 
-    mutate(method = factor(method, levels = c("bglm_t", "bglm_de", "blasso",
-                                              "bglm_spline_t", "bglm_spline_de", "blasso_spline",
-                                              "cosso", "acosso", "mgcv"
-    )))
-  
-  
-      total_dat %>% 
-        select(p, method, starts_with("test.")) %>% #head
-        # pivot_longer(cols = starts_with("test.")) %>%
-        # mutate(name = str_remove(name, pattern = fixed("test."))) %>% 
-        rename_at(vars(dplyr::starts_with("test.")), ~str_remove(., pattern = fixed("test."))) %>%
-        # select(p,method, {{measures}}) %>% 
-        mutate(p = factor(p)) %>% 
-        group_by(p, method) %>% 
-        dplyr::summarise(across(everything(),list(mean = mean, sd = sd), na.rm = TRUE, .names = "{.col}.{.fn}")) %>% 
-        ungroup
-}
---
+++ packages.R
@@ -0,0 +1,21 @@
+## library() calls go here
+library(conflicted)
+library(dotenv)
+library(targets)
+library(tarchetypes)
+
+
+library(dplyr)
+library(tidyverse)
+library(rticles)
+library(gtsummary)
+library(here)
+library(mgcv)
+library(broom)
+library(unglue)
+library(knitr)
+library(ggpubr)
+library(xtable)
+
+
+conflict_prefer("filter", "dplyr")
